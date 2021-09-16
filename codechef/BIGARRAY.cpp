63b8e157290e99c9806c7f8a9dbab6ead05fd5a1
Freakqency <suryashankar649@gmail.com>
2021-09-16 16:02:42 +0530

    SEPT Starters Codechef

--- codechef/BIGARRAY.cpp
+++ codechef/BIGARRAY.cpp
@@ -2,26 +2,20 @@
 
 using namespace std;
 
-int solve(int n, int s) {
-    int target = 0, res = -1;
-    for (int i = 1; i <= n; i++) {
-        target += i;
-        if (target > s) break;
-    }
-    for (int i = 1; i <= n; i++) {
-        if (target - i == s) {
-            res = i;
-            break;
-        }
-    }
-    return res;
+#define ll long long
+
+ll solve(ll n, ll s) {
+    ll tot =  (n * (n + 1) ) / 2;
+    ll x = tot - s;
+    if (x >= 1 && x <= n) return x;
+    return -1;
 }
 
 int main() {
     ios_base::sync_with_stdio(false); cin.tie(NULL);    
-    int t; cin >> t;
+    ll t; cin >> t;
     while (t--) {
-        int n, s; cin >> n >> s;
+        ll n, s; cin >> n >> s;
         cout << solve(n, s) << '\n';
     }
     return 0;
